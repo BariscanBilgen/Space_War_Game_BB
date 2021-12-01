b93f93e9070b77bfeee117d71cac943d5813764f
Barışcan Bilgen <91004987+BariscanBilgen@users.noreply.github.com>
2021-12-01 11:55:32 +0300

    Add files via upload

---
+++ SpaceWarGame/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace SpaceWarGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox_Galaxy = new System.Windows.Forms.PictureBox();
+            this.pictureBox_PlayerShip = new System.Windows.Forms.PictureBox();
+            this.label_Point = new System.Windows.Forms.Label();
+            this.timer_Player = new System.Windows.Forms.Timer(this.components);
+            this.timer_BulletThrow = new System.Windows.Forms.Timer(this.components);
+            this.timer_ShootEnemy = new System.Windows.Forms.Timer(this.components);
+            this.timer_EnemyCreate = new System.Windows.Forms.Timer(this.components);
+            this.timer_BulletControl = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Galaxy)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_PlayerShip)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox_Galaxy
+            // 
+            this.pictureBox_Galaxy.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox_Galaxy.Image = global::SpaceWarGame.Properties.Resources.SpaceBackground;
+            this.pictureBox_Galaxy.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox_Galaxy.Name = "pictureBox_Galaxy";
+            this.pictureBox_Galaxy.Size = new System.Drawing.Size(433, 635);
+            this.pictureBox_Galaxy.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox_Galaxy.TabIndex = 0;
+            this.pictureBox_Galaxy.TabStop = false;
+            // 
+            // pictureBox_PlayerShip
+            // 
+            this.pictureBox_PlayerShip.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox_PlayerShip.Image = global::SpaceWarGame.Properties.Resources.SpaceShip;
+            this.pictureBox_PlayerShip.Location = new System.Drawing.Point(152, 453);
+            this.pictureBox_PlayerShip.Name = "pictureBox_PlayerShip";
+            this.pictureBox_PlayerShip.Size = new System.Drawing.Size(128, 91);
+            this.pictureBox_PlayerShip.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox_PlayerShip.TabIndex = 1;
+            this.pictureBox_PlayerShip.TabStop = false;
+            // 
+            // label_Point
+            // 
+            this.label_Point.AutoSize = true;
+            this.label_Point.BackColor = System.Drawing.Color.Transparent;
+            this.label_Point.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label_Point.ForeColor = System.Drawing.Color.White;
+            this.label_Point.Location = new System.Drawing.Point(33, 30);
+            this.label_Point.Name = "label_Point";
+            this.label_Point.Size = new System.Drawing.Size(93, 24);
+            this.label_Point.TabIndex = 2;
+            this.label_Point.Text = "PUAN = 0";
+            // 
+            // timer_Player
+            // 
+            this.timer_Player.Interval = 1;
+            this.timer_Player.Tick += new System.EventHandler(this.timer_Player_Tick);
+            // 
+            // timer_BulletThrow
+            // 
+            this.timer_BulletThrow.Interval = 10;
+            this.timer_BulletThrow.Tick += new System.EventHandler(this.timer_BulletThrow_Tick);
+            // 
+            // timer_ShootEnemy
+            // 
+            this.timer_ShootEnemy.Interval = 50;
+            this.timer_ShootEnemy.Tick += new System.EventHandler(this.timer_ShootEnemy_Tick);
+            // 
+            // timer_EnemyCreate
+            // 
+            this.timer_EnemyCreate.Interval = 2000;
+            this.timer_EnemyCreate.Tick += new System.EventHandler(this.timer_EnemyCreate_Tick);
+            // 
+            // timer_BulletControl
+            // 
+            this.timer_BulletControl.Tick += new System.EventHandler(this.timer_BulletControl_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(433, 635);
+            this.Controls.Add(this.label_Point);
+            this.Controls.Add(this.pictureBox_PlayerShip);
+            this.Controls.Add(this.pictureBox_Galaxy);
+            this.Name = "Form1";
+            this.Text = "Space War Game BB";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Galaxy)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_PlayerShip)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox_Galaxy;
+        private System.Windows.Forms.PictureBox pictureBox_PlayerShip;
+        private System.Windows.Forms.Label label_Point;
+        private System.Windows.Forms.Timer timer_Player;
+        private System.Windows.Forms.Timer timer_BulletThrow;
+        private System.Windows.Forms.Timer timer_ShootEnemy;
+        private System.Windows.Forms.Timer timer_EnemyCreate;
+        private System.Windows.Forms.Timer timer_BulletControl;
+    }
+}
+
---
+++ SpaceWarGame/Form1.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SpaceWarGame
+{
+    public partial class Form1 : Form
+    {
+        int FormHeight = 700, FormWidth = 500;
+
+        // oyuncu koordinat
+        int Player_X = 200, Player_Y = 400;
+        int PlayerSpeed_Horizontal = 0, PlayerSpeed_Vertical = 0;
+        int point;
+        List<PictureBox> Bullets = new List<PictureBox>();
+        int BulletSpeed = 10;
+        List<PictureBox> Enemys = new List<PictureBox>();
+        int EnemySpeed = 2;
+        Random rnd = new Random();
+        private Bitmap Image;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Height = FormHeight;
+            this.Width = FormWidth;
+
+            // objeleri galaxy içine aldık.
+            label_Point.Parent = pictureBox_Galaxy;
+            pictureBox_PlayerShip.Parent = pictureBox_Galaxy;
+            
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            int speed = 3;
+            switch (e.KeyCode)
+            {
+                case Keys.Left: PlayerSpeed_Horizontal -= speed; break;
+                case Keys.Right: PlayerSpeed_Horizontal += speed; break;
+                case Keys.Up: PlayerSpeed_Vertical -= speed; break;
+                case Keys.Down: PlayerSpeed_Vertical += speed; break;
+                case Keys.Enter:
+                    point = 0;
+                    timer_Player.Start();
+                    timer_BulletThrow.Start();
+                    timer_EnemyCreate.Start();
+                    timer_ShootEnemy.Start();
+                    timer_BulletControl.Start();
+                    break;
+                case Keys.Space:
+                    BulletCreat();
+                    break;
+            }
+
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Left: PlayerSpeed_Horizontal = 0; break;
+                case Keys.Right: PlayerSpeed_Horizontal = 0; break;
+                case Keys.Up: PlayerSpeed_Vertical = 0; break;
+                case Keys.Down: PlayerSpeed_Vertical = 0; break;
+            }
+        }
+
+        private void timer_BulletThrow_Tick(object sender, EventArgs e)
+        {
+            BulletThrow();
+        }
+
+        private void timer_Player_Tick(object sender, EventArgs e)
+        {
+            Player_X += PlayerSpeed_Horizontal; 
+            Player_Y += PlayerSpeed_Vertical;
+            pictureBox_PlayerShip.Location = new Point(Player_X, Player_Y);
+        }
+
+        // Mermi oluşturuyoruz.
+        public void BulletCreat()
+        {
+            PictureBox Bullet = new PictureBox();
+
+
+              Bullet.Size = new Size(55, 25);
+           
+             Bullet.SizeMode = PictureBoxSizeMode.StretchImage;
+             Bullet.Image = Properties.Resources.bullet;
+             Bullet.BackColor = Color.Transparent;
+
+            int BulletLocX = pictureBox_PlayerShip.Left + pictureBox_PlayerShip.Width / 2 - 5;
+            int BulletLocY = pictureBox_PlayerShip.Top - 1;
+            Bullet.Location = new Point(BulletLocX, BulletLocY);
+            Controls.Add(Bullet);
+            Bullet.Parent = pictureBox_Galaxy;
+            Bullet.BringToFront(); // galaxy resminin üstüne çıkardık.
+            Bullets.Add(Bullet);
+        }
+
+        private void timer_ShootEnemy_Tick(object sender, EventArgs e)
+        {
+            ShootEnemy();
+        }
+
+        private void timer_EnemyCreate_Tick(object sender, EventArgs e)
+        {
+            EnemyCreate();
+        }
+
+        private void timer_BulletControl_Tick(object sender, EventArgs e)
+        {
+            for (int b = 0; b < Bullets.Count; b++)
+            {
+                for (int s = 0; s < Enemys.Count; s++)
+                {
+                    try
+                    {
+                        if (Bullets[b].Bounds.IntersectsWith(Enemys[s].Bounds))
+                        {
+                            point += 1;
+                            label_Point.Text = "PUAN = " + point.ToString();
+                            pictureBox_Galaxy.Controls.Remove(Bullets[b]);
+                            Bullets.Remove(Bullets[b]);
+                            pictureBox_Galaxy.Controls.Remove(Enemys[s]);
+                            Enemys.Remove(Enemys[s]);
+
+                            // ram temizleme
+                            GC.Collect(); // çöp toplayıcı
+                            GC.WaitForPendingFinalizers();// çöp yok et 
+                        }
+                    }
+                    catch (ArgumentOutOfRangeException)
+                    {
+                        return;
+                    }
+                    
+                }
+            }
+        }
+
+        public void BulletThrow()
+        {
+            for (int i = 0; i < Bullets.Count; i++)
+            {
+                Bullets[i].Top -= BulletSpeed;
+            }
+        }
+
+        public void EnemyCreate()
+        {
+            int Locations = rnd.Next(0, FormWidth - 50);
+            PictureBox Enemy = new PictureBox();
+            Enemy.Size = new Size(100, 80);
+            Enemy.SizeMode = PictureBoxSizeMode.StretchImage;
+            Enemy.Image = Properties.Resources.EnemyShip;
+            Enemy.BackColor = Color.Transparent;
+            Enemy.Location = new Point(Locations, 0);
+            Enemy.Visible = true;
+            Controls.Add(Enemy);
+            Enemy.Parent = pictureBox_Galaxy;
+            Enemy.BringToFront();
+            Enemys.Add(Enemy);
+        }
+
+        public void ShootEnemy()
+        {
+            for (int i = 0; i < Enemys.Count; i++)
+            {
+                Enemys[i].Top += EnemySpeed;
+                if (Enemys[i].Top >= FormHeight-10 || Enemys[i].Bounds.IntersectsWith(pictureBox_PlayerShip.Bounds))
+                {
+                    GameFinish();
+                }
+            }
+        }
+
+        public void GameFinish()
+        {
+            timer_Player.Stop();
+            timer_BulletThrow.Stop();
+            timer_BulletControl.Stop();
+            timer_EnemyCreate.Stop();
+            timer_ShootEnemy.Stop();
+
+            MessageBox.Show("OYUN BİTTİ...");
+
+        }
+    }
+    }
+
+
